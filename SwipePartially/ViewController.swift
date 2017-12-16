3d71c96827df25679034a45a4ddd60a341f0acc1
MinaFujisawa <fujimina37@gmail.com>
2017-12-15 19:30:33 -0800

    first commit

---
+++ SwipePartially/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipePartially
+//
+//  Created by MINA FUJISAWA on 2017/12/15.
+//  Copyright © 2017 MINA FUJISAWA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
